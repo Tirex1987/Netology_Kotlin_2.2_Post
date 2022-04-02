5ebfb9b4a11719a6b1994a15635ca0bd5ae1bc17
Tirex1987 <tirex1987@yandex.ru>
2022-04-02 20:01:29 +0300

    Initial commit

---
+++ src/main/kotlin/Post.kt
@@ -0,0 +1,41 @@
+package post
+
+import java.util.*
+
+data class Post(
+    var id: Int,                //Идентификатор записи
+    val ownerId: Int,           //Идентификатор владельца стены, на которой размещена запись
+    val fromId: Int,            //Идентификатор автора записи (от чьего имени опубликована запись)
+    val createdBy: Int,         //Идентификатор администратора, который опубликовал запись
+    val date: Int,              //Время публикации записи в формате
+    var text: String,           //Текст записи
+    val replyOwnerId: Int,      //Идентификатор владельца записи, в ответ на которую была оставлена текущая
+    val replyPostId: Int,       //Идентификатор записи, в ответ на которую была оставлена текущая
+    val friendsOnly: Boolean,   // true, если запись была создана с опцией «Только для друзей»
+    val comments: Any,       //Информация о комментариях к записи
+    val copyright: Any,      //Источник материала
+    val likes: Any,          //Информация о лайках к записи
+    val reposts: Any,        //Информация о репостах записи («Рассказать друзьям»)
+    val views: Any,          //Информация о просмотрах записи
+    val postType: String,       //Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest
+    val signerId: Int,          //Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
+    var canPin: Boolean,        //Информация о том, может ли текущий пользователь закрепить запись
+    var canDelete: Boolean,     //Информация о том, может ли текущий пользователь удалить запись
+    var canEdit: Boolean,       //Информация о том, может ли текущий пользователь редактировать запись
+    var isPinned: Boolean,      //Информация о том, что запись закреплена
+    var markedAsAds: Boolean,   //Информация о том, содержит ли запись отметку «реклама»
+    var isFavorite: Boolean,    //true, если объект добавлен в закладки у текущего пользователя
+    var donut: Any,          //Информация о записи VK Donut
+    var postponedId: Int        //Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере
+){
+    constructor(ownerId: Int, text: String, date: Int, friendsOnly: Boolean, markedAsAds: Boolean) : this(
+        id = 0, ownerId = ownerId, fromId = ownerId,
+        createdBy = 0, date = date, text, replyOwnerId = 0,
+        replyPostId = 0, friendsOnly = friendsOnly,
+        comments = Any(), copyright = Any(), likes = Any(),
+        reposts = Any(), views = Any(), postType = "post",
+        signerId = 0, canPin = true, canDelete = true, canEdit = true,
+        isPinned = false, markedAsAds = markedAsAds, isFavorite = false,
+        donut = Any(), postponedId =0
+    )
+}
---
+++ src/main/kotlin/WallService.kt
@@ -0,0 +1,22 @@
+package post
+
+class WallService {
+    private var posts = emptyArray<Post>()
+    private var lastId = 0
+
+    fun add(post: Post): Post{
+        post.id = ++lastId
+        posts += post
+        return posts.last()
+    }
+
+    fun update(post:Post): Boolean{
+        for ((index, targetPost) in posts.withIndex()){
+            if (targetPost.id == post.id){
+                posts[index] = post.copy(id = targetPost.id, date = targetPost.date)
+                return true
+            }
+        }
+        return false
+    }
+}
